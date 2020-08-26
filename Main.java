fb403ab1f2c7ba451c12187e4368ff80106e96e5
Kirill <kiruxastar@gmail.com>
2020-08-26 19:17:39 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int Ticket = 32818_00; // с копейками
+        int Bonus = Ticket /100 / 20;
+        System.out.println(Bonus);
+    }
+}
